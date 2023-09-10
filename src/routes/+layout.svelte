eb835e7b3be0409c3aba9488a106b7160fc445de
Avery Rosenblum-O'Connor <contact@nekodjin.org>
2023-09-10 04:37:03 +0000

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+
+    import "../app.css";
+</script>
+
+<svelte:head>
+    <title>{$page.data.pageTitle}</title>
+</svelte:head>
+
+<slot />
---
+++ src/routes/+layout.ts
@@ -0,0 +1,7 @@
+export const prerender = true;
+
+export async function load() {
+    return {
+        pageTitle: "toki.club/~avie",
+    };
+}
